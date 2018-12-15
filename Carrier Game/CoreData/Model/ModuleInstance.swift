3b32491e3e923e04e18f30938067e69713845185
sdot <sean.g.young@gmail.com>
2018-12-15 18:49:55 -0500

    Minor clean up.

--- Carrier Game/CoreData/Model/ModuleInstance.swift
+++ Carrier Game/CoreData/Model/ModuleInstance.swift
@@ -29,33 +29,17 @@ extension ModuleInstance {
         return GridRect(origin: origin, size: size)
     }
     
-//    var rect: CGRect {
-//        return CGRect(x: placement.origin.x, y: placement.origin.y, width: CGFloat(placement.blueprint.size.x), height: CGFloat(placement.blueprint.size.y))
-//    }
-    
     var xyEntranceCoords: [GridPoint3] {
         // Translate all open coords by origin
         let openCoords = blueprint.xyOpenCoords.map { GridPoint3($0 + placement.origin, Int(deck.blueprint.position)) }
-        // TODO: JUST USE RECT?
-        let gridRect = GridRect(origin: GridPoint3(placement.origin, Int(deck.blueprint.position)), size: GridPoint3(Int(placement.blueprint.size.x), Int(placement.blueprint.size.y), 1))
+        
         // Filter those that open at barriers
         return openCoords.filter { coord in
-            if coord.x == gridRect.xRange.first || coord.x == gridRect.xRange.last { return true }
-            if coord.y == gridRect.yRange.first || coord.y == gridRect.yRange.last { return true }
+            if coord.x == rect.xRange.first || coord.x == rect.xRange.last { return true }
+            if coord.y == rect.yRange.first || coord.y == rect.yRange.last { return true }
             return false
         }
     }
-    
-//    var xyEntranceCoords: [float3] {
-//        // Translate all open coords by origin
-//        let openCoords = blueprint.xyOpenCoords.map { float3(point: $0 + placement.origin, vertical: deck.blueprint.position) }
-//        // Filter those that open at barriers
-//        return openCoords.filter { coord in
-//            if CGFloat(coord.x) == rect.minX || CGFloat(coord.x) == (rect.maxX - 1) { return true }
-//            if CGFloat(coord.y) == rect.minY || CGFloat(coord.y) == (rect.maxY - 1) { return true }
-//            return false
-//        }
-//    }
     
     var zEntranceCoords: [GridPoint3] {
         // All z open coords are entrances to another deck so just map
